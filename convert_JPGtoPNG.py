83ea064125ecb3e356e3b132a5ae03f7e6c780b5
viljamicode <viljami.rings@gmail.com>
2022-12-08 13:12:40 +0200

    Adding comment

--- convert_JPGtoPNG.py
+++ convert_JPGtoPNG.py
@@ -1,16 +1,7 @@
 # This is a simple script that uses Pillow (Python Imaging Library fork for python3)
 # to convert JPGs to PNGs within a folder.
 # You should have Pillow installed. (https://pillow.readthedocs.io/en/stable/installation.html)
-
-
-# To run:
-# 1.  Open the cmd/terminal to the folder where "convert_JPGtoPNG.py" is
-#     and make sure it has a subfolder with the JPG/JPGs that you wish to convert to PNG.
-# 2.  Type: "python convert_JPGtoPNG.py 'existing_images_folder/' 'new_images_folder/'" with no quotation marks.
-# 2.5.Change "existing_images_folder" and "new_images_folder" to the correct folder names.
-#     The output folder doesn't have to be an existing folder, since the script will create it with the name
-#     that you have written into the cmd/terminal (for example "new_images_folder").
-# 3.  Press ENTER and your JPG images have been converted into PNGs and saved in the new folder.
+# Check README.md for further instructions.
 
 import sys
 import os
